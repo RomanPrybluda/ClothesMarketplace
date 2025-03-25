29eef9080fd75370a655f8e834c7c365d21dd8bb
Roman Prybluda <romanodua@gmail.com>
2025-03-25 23:16:56 +0200

    add new exeptions

--- Domain/Exceptions/CustomExceptionType.cs
+++ Domain/Exceptions/CustomExceptionType.cs
@@ -8,6 +8,8 @@
         IsAlreadyExists = 4,
         UserIsAlreadyExists = 5,
         InvalidInputData = 6,
-        ProductAlreadyExists = 7
+        ProductAlreadyExists = 7,
+        InvalidData = 8
+
     }
 }
