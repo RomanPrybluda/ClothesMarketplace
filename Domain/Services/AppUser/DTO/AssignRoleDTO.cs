cbb9e28a960141ec114ab062b6d33c06ccc87563
Solohub <nastia.sologub11113@gmail.com>
2025-03-29 17:56:28 +0200

    Add AppUser dto

---
+++ Domain/Services/AppUser/DTO/AssignRoleDTO.cs
@@ -0,0 +1,4 @@
+public class AssignRoleDTO
+{
+    public string? RoleName { get; set; }
+}
---
+++ Domain/Services/AppUser/DTO/UserUpdateDTO.cs
@@ -0,0 +1,6 @@
+    public class UserUpdateDTO
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public int? Age { get; set; }
+    }
