b7e55c8878eb2d64e8c8d0a48f7d3d5f507bb637
OlehKozlovskyi <oleg.koylovskiy.15-p@ukr.net>
2025-04-01 21:13:42 +0300

    Create FileValidator

---
+++ Domain/Validators/FileValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Validators
+{
+    public class FileValidator :AbstractValidator<IFormFile>
+    {
+        public FileValidator()
+        {
+            RuleFor(file => file.Length).LessThanOrEqualTo(5242880).WithMessage("File size must be less than or equal to 5 MB.");
+            RuleFor(file => Path.GetExtension(file.Name)).Matches(@"\.(jpg|jpeg|png|webp)$").WithMessage("File type must be jpeg, jpg, png or webp.");
+            RuleFor(file => file).NotNull().WithMessage("At least one image is required.");
+        }
+    }
+}
