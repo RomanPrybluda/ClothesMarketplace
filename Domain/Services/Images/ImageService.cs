36d79a7a88c7b0fddbdcf73f348570c6e780053d
OlehKozlovskyi <oleg.koylovskiy.15-p@ukr.net>
2025-04-01 21:13:21 +0300

    Start implement ImagesService

---
+++ Domain/Services/Images/ImageService.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services.Images
+{
+    public class ImageService
+    {
+        private readonly IHostEnvironment _hostEnvironment;
+        private readonly IValidator<IFormFile> _fileValidator;
+
+        public ImageService(IHostEnvironment environment, IValidator<IFormFile> fileValidator)
+        {
+            _hostEnvironment = environment;
+            _fileValidator = fileValidator;
+        }
+
+        public async Task<string> SaveImageAsync(IFormFile file)
+        {
+            var validationResult = _fileValidator.Validate(file);
+            if (validationResult.IsValid)
+            {
+
+            }
+        }
+    }
+}
