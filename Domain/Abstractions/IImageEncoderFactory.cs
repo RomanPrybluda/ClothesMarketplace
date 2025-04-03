8ca2cf72f041c02e19e0d93929d0533f6b33bcef
OlehKozlovskyi <oleg.koylovskiy.15-p@ukr.net>
2025-04-03 19:57:00 +0300

    Add ImageEncoderFactory

---
+++ Domain/Abstractions/IImageEncoderFactory.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using SixLabors.ImageSharp.Formats;
+
+namespace Domain.Abstractions
+{
+    public interface IImageEncoderFactory
+    {
+        IImageEncoder GetEncoder(IFormFile file);
+    }
+}
--- Domain/Services/Images/ImageService.cs
+++ Domain/Services/Images/ImageService.cs
@@ -1,9 +1,14 @@
-﻿using FluentValidation;
+﻿using Domain.Abstractions;
+using FluentValidation;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Hosting;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.Formats;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,20 +18,40 @@ namespace Domain.Services.Images
     {
         private readonly IHostEnvironment _hostEnvironment;
         private readonly IValidator<IFormFile> _fileValidator;
+        private readonly IImageEncoderFactory _encoderFactory;
 
-        public ImageService(IHostEnvironment environment, IValidator<IFormFile> fileValidator)
+        public ImageService(IHostEnvironment environment, IValidator<IFormFile> fileValidator, IImageEncoderFactory encoderFactory)
         {
             _hostEnvironment = environment;
             _fileValidator = fileValidator;
+            _encoderFactory = encoderFactory;
         }
 
-        public async Task<string> SaveImageAsync(IFormFile file)
+        public async Task<string> UploadImageFileAsync(IFormFile file)
         {
-            var validationResult = _fileValidator.Validate(file);
+            string path = Path.Combine(_hostEnvironment.ContentRootPath, "wwwroot", "images");
+            Directory.CreateDirectory(path);
+            var validationResult = await _fileValidator.ValidateAsync(file);
+            var fileName = Path.GetRandomFileName() + Path.GetExtension(file.FileName);
+            var filePath = Path.Combine(path, fileName);
             if (validationResult.IsValid)
             {
 
+                using var stream = new FileStream(filePath, FileMode.Create);
+                await file.CopyToAsync(stream);
+                return $"/images/{fileName}";
             }
+            else
+            {
+                throw new CustomException(CustomExceptionType.InvalidInputData, validationResult.Errors.First().ErrorMessage);
+            }
+        }
+
+        private async Task<bool> CompressImage(IFormFile imageFile)
+        {
+            using var imageStream = imageFile.OpenReadStream();
+            using var image = await Image.LoadAsync(imageStream);
+
         }
     }
 }
