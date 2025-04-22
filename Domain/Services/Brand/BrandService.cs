1089bd6284b80a28d99101d51c537097a2a18769
OlehKozlovskyi <oleg.koylovskiy.15-p@ukr.net>
2025-04-22 21:55:30 +0300

    Finish to implement BrandServcie
    Add methods that allows update and remove categories

--- Domain/Services/Brand/BrandService.cs
+++ Domain/Services/Brand/BrandService.cs
@@ -1,12 +1,6 @@
 ﻿using DAL;
 using Domain.Abstractions;
-using Domain.Services.Brand.DTO;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Services.Brand
 {
@@ -15,7 +9,7 @@ namespace Domain.Services.Brand
         private readonly ClothesMarketplaceDbContext _context;
         private readonly IImageService _imageService;
 
-        public BrandService(ClothesMarketplaceDbContext context, IImageService imageService) 
+        public BrandService(ClothesMarketplaceDbContext context, IImageService imageService)
         {
             _context = context;
             _imageService = imageService;
@@ -26,7 +20,7 @@ namespace Domain.Services.Brand
             var brands = await _context.Brands.ToListAsync();
             if (!brands.Any())
                 throw new CustomException(CustomExceptionType.NotFound, "No brands found");
-            
+
             List<BrandDTO> brandsResponse = new();
             foreach (var brand in brands)
             {
@@ -52,7 +46,7 @@ namespace Domain.Services.Brand
         {
             var existingBrand = await _context.Brands.FirstOrDefaultAsync(
                 b => b.Name.ToLower() == request.Name.ToLower());
-            
+
             if (existingBrand != null)
                 throw new CustomException(CustomExceptionType.IsAlreadyExists, $"Brand '{request.Name}' already exists.");
 
@@ -63,6 +57,34 @@ namespace Domain.Services.Brand
 
             var createdBrand = await _context.Brands.FindAsync(brand.Id);
             return BrandDTO.FromBrand(createdBrand);
+        }
+
+        public async Task<BrandDTO> UpdateBrandAsync(Guid Id, UpdateBrandDTO request)
+        {
+            var existingBrand = await _context.Brands.FindAsync(Id);
+
+            if (existingBrand is null)
+                throw new CustomException(CustomExceptionType.NotFound, $"No brand found with ID {Id}");
+
+            existingBrand.ImageName = await _imageService.UploadImageAsync(request.Image);
+            existingBrand.Name = request.Name;
+
+            _context.Update(existingBrand);
+            await _context.SaveChangesAsync();
+
+            var updatedBrand = await _context.Brands.FindAsync(existingBrand.Id);
+            return BrandDTO.FromBrand(updatedBrand);
+        }
+
+        public async Task DeleteBrandAsync(Guid Id)
+        {
+            var brand = await _context.Brands.FindAsync(Id);
+
+            if (brand is null)
+                throw new CustomException(CustomExceptionType.NotFound, $"No brand found with ID {Id}");
+
+            _context.Brands.Remove(brand);
+            await _context.SaveChangesAsync();
         }
     }
 }
---
+++ Domain/Services/Brand/DTO/UpdateBrandDTO.cs
@@ -0,0 +1,12 @@
+﻿using DAL;
+using Microsoft.AspNetCore.Http;
+
+namespace Domain
+{
+    public class UpdateBrandDTO
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public IFormFile? Image { get; set; }
+    }
+}
