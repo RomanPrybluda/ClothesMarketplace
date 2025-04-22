b7d4e66afa1767d7d63afd4c678094791e10ca12
OlehKozlovskyi <oleg.koylovskiy.15-p@ukr.net>
2025-04-22 20:44:55 +0300

    Add images into category
    You can add images into categories, you can also update categories images.

--- Domain/Services/Category/DTO/CreateCategoryDTO.cs
+++ Domain/Services/Category/DTO/CreateCategoryDTO.cs
@@ -1,10 +1,13 @@
 ﻿using DAL;
+using Microsoft.AspNetCore.Http;
 
 namespace Domain
 {
     public class CreateCategoryDTO
     {
         public string Name { get; set; } = string.Empty;
+
+        public IFormFile Image { get; set; }
 
         public static Category ToCategory(CreateCategoryDTO request)
         {
--- Domain/Services/Category/DTO/UpdateCategoryDTO.cs
+++ Domain/Services/Category/DTO/UpdateCategoryDTO.cs
@@ -1,10 +1,13 @@
 ﻿using DAL;
+using Microsoft.AspNetCore.Http;
 
 namespace Domain
 {
     public class UpdateCategoryDTO
     {
         public string Name { get; set; } = string.Empty;
+
+        public IFormFile? Image { get; set; }
 
         public void UpdateCategory(Category category)
         {
