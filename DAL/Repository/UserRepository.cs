1105651991aba8585abc3c77e2e887ce8c812c21
OlehKozlovskyi <oleg.koylovskiy.15-p@ukr.net>
2025-04-26 16:42:51 +0300

    Staret refactoring AuthService

---
+++ DAL/Repository/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    public class UserRepository(UserManager<AppUser> userManager)
+    {
+        public async Task<AppUser> FindByEmail(string email)
+        {
+            return await userManager.FindByEmailAsync(email);
+        }
+
+        public async Task<AppUser> FindByName(string userName)
+        {
+            return await userManager.FindByNameAsync(userName);
+        }
+    }
+}
