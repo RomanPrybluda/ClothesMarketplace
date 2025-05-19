4d6e602fee3a649edc5d158f360bc67d0adf85d4
Roman Prybluda <romanodua@gmail.com>
2025-05-19 10:12:54 +0300

    add services

---
+++ Domain/Services/Color/DTO/UpdateColorDTO.cs
@@ -0,0 +1,14 @@
+﻿using DAL;
+
+namespace Domain
+{
+    public class UpdateColorDTO
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public void UpdateColor(Color color)
+        {
+            color.Name = Name;
+        }
+    }
+}
---
+++ Domain/Services/ForWhom/DTO/UpdateForWhomDTO.cs
@@ -0,0 +1,14 @@
+﻿using DAL;
+
+namespace Domain
+{
+    public class UpdateForWhomDTO
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public void UpdateForWhom(ForWhom forWhom)
+        {
+            forWhom.Name = Name;
+        }
+    }
+}
